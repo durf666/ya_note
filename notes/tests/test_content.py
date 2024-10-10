c7d7bd8d8484247aafe1fce2c1ade66471eb8e53
durf <me@example.com>
2024-10-10 11:49:58 +0300

    routes tes finished. content testing

---
+++ notes/tests/test_content.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from notes.forms import NoteForm
+from notes.models import Note
+
+User = get_user_model()
+
+class TestNotesList(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        self.user = User.objects.create(username='testuser')
+        self.client.force_login(self.user)
+
+    # def test_note_list(self):
+    #     url = reverse('notes:list')
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, 200)
